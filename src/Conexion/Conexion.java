6f9ee391e98960cecd90e3bd1f1a68231ffe8312
anthony <alexis310595@gmail.com>
2018-09-05 16:13:04 -0500

    mis cambios

---
+++ src/Conexion/Conexion.java
@@ -0,0 +1,29 @@
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    private static String bd="almacenbd";
+    private static String USER = "root";
+    private static String PASSWORD = "triangulo2018";
+    private static String url = "jdbc:mysql://localhost/"+bd+ "?user=" + USER + "&password=" + PASSWORD + "&useSSL=false";;
+    private static Connection con;
+    
+    public static Connection geconnection (){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection(url,USER,PASSWORD);
+            
+        }catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"error" +e.getMessage());
+        }
+        return con;
+    }
+    /*public static void main(String[] args) {
+        Connection con;
+        con= geconnection();
+        System.out.println("estado: "+con);
+    }*/
+}
